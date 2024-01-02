2f9268693a91db50837647339c6774e34cf80ad7
ghost <jhonsnow751@gmail.com>
2024-01-02 08:16:01 +0100

    SCvhjshbfsbgcvhgghcgh

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ socketUtils.connection(io);
 
 const socketIoMiddleware = (req, res, next) => {
   req.io = io;
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
   next();
 };
 
